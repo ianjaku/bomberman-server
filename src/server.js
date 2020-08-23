497762829a6ed1fbde091a0666b0642ae35d02fd
Ian <ian@invacto.com>
2020-08-23 13:19:20 +0200

    init

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use("/static", express.static("public"))
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Bombarman game server")
+})
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`)
+})
