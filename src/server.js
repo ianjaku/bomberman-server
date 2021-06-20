dbc275cabef959145df62450d7ce1e3d3add0be3
Ian <ian@invacto.com>
2021-06-20 12:25:05 +0200

    episode 6

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 4000
 app.use("/static", express.static("public"))
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Bombarman game server")
+  res.send("Welcome to the Bomberman game server")
 })
 
 app.listen(port, () => {
